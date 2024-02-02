3b0b1bd743ab530081038cbde45656d50d269130
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-02-02 14:46:54 +0700

    set CRUD and validator for books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Header,
+  Res,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -8,27 +18,57 @@ export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.booksService.create(createBookDto);
+  @Header('Content-Type', 'application/json')
+  async create(@Body() createBookDto: CreateBookDto, @Res() res) {
+    return this.booksService
+      .create(createBookDto)
+      .then((ok) => {
+        return res.json({
+          message: 'OK',
+          data: ok,
+        });
+      })
+      .catch((error) => {
+        return res.status(error.status).json({
+          error: error.response,
+        });
+      });
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.booksService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.booksService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.booksService.update(+id, updateBookDto);
+  @Header('Content-Type', 'application/json')
+  async update(
+    @Param('id') id: string,
+    @Body() updateBookDto: UpdateBookDto,
+    @Res() res,
+  ) {
+    return this.booksService
+      .update(+id, updateBookDto)
+      .then((ok) => {
+        return res.json({
+          message: 'OK',
+          data: ok,
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error: error.response,
+        });
+      });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string) {
     return this.booksService.remove(+id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma.service';
+import { Book } from '@prisma/client';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createBookDto: CreateBookDto) {
+    const isReady = await this.prisma.book.findUnique({
+      where: {
+        title: createBookDto.title,
+      },
+    });
+
+    if (isReady) throw new ConflictException('Title book is already exist');
+
+    const saveBook = await this.prisma.book.create({
+      data: {
+        ...createBookDto,
+        publishedAt: new Date(createBookDto.publishedAt),
+      },
+    });
+    return saveBook;
   }
 
-  findAll() {
-    return `This action returns all books`;
+  async findAll(): Promise<Book[]> {
+    return this.prisma.book.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
+  async findOne(id: number): Promise<Book> {
+    const book = await this.prisma.book.findUnique({
+      where: { id },
+    });
+
+    return book;
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+  async update(id: number, updateBookDto: UpdateBookDto) {
+    const book = await this.prisma.book.findUnique({
+      where: { id },
+    });
+
+    if (!book) throw new NotFoundException('Data yang kamu cari tidak ada');
+
+    return this.prisma.book.update({
+      where: { id },
+      data: {
+        ...updateBookDto,
+        publishedAt: new Date(updateBookDto.publishedAt),
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+  async remove(id: number): Promise<Book> {
+    const book = await this.prisma.book.findUnique({
+      where: { id }
+    })
+
+    if (!book) throw new NotFoundException('Data yang kamu cari tidak ada')
+
+    return this.prisma.book.delete({
+      where: {id}
+    })
   }
 }
